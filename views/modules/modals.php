2a13baab736ee8454a76eb4122ac24d9f0173831
William M <w83mejia@gmail.com>
2025-10-01 16:47:37 -0500

    Separé los módulos

---
+++ views/modules/modals.php
@@ -0,0 +1,43 @@
+<!-- Button trigger modal -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+
+    <form action="" method="post">
+        <div class="modal-content">
+
+        <!-- header -->
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+
+        <!-- body -->
+        <div class="modal-body">
+
+            <div class="mb-3">
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="name@example.com">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="*******">
+            </div>
+
+        </div>
+
+        <!-- footer -->
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Cerrar</button>
+
+            <button type="submit" class="btn btn-info rounded-pill">Ingresar</button>
+        </div>
+
+        </div>
+    </form>
+
+    
+  </div>
+</div>
---
+++ views/modules/top.php
@@ -0,0 +1,58 @@
+<div class="container-fluid" style="background: #000; color: #fff">
+
+    <div class="container">
+
+        <div class="d-flex justify-content-between small">
+
+            <!-- Izquierda -->
+            <div class="p-2">
+                <!-- íconos -->
+                <div class="d-flex justify-content-center">
+
+                    <!-- facebook -->
+                    <div class="p-2">
+                        <a href="https://facebook.com" class="text-white" target="blank">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </div>
+
+                    <!-- Youtube -->
+                    <div class="p-2">
+                        <a href="https://youtube.com" class="text-white" target="blank">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+
+                    <!-- Instagram -->
+                    <div class="p-2">
+                        <a href="https://instagram.com" class="text-white" target="blank">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Derecha -->
+            <div class="p-2">
+               <div class="d-flex justify-content-center">
+                    <!-- ingresar y crear cuenta -->
+                    <div class="p-2">
+                        <a href="#" target="_blank" class="text-white"  data-bs-toggle="modal" data-bs-target="#exampleModal"> Igresar</a>
+                    </div>
+
+                    <div class="p-2">
+                        |
+                    </div>
+
+                    <div class="p-2">
+                        <a href="#" target="_blank" class="text-white"> Crear Cuenta</a>
+                    </div>
+               </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
