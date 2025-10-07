9cebf49fa264f5d8605a792124f98ee8d65b729c
William M <w83mejia@gmail.com>
2025-10-07 12:57:07 -0500

    navbar completado

--- views/modules/navbar.php
+++ views/modules/navbar.php
@@ -1,168 +1,184 @@
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
-    <div class="container">
-      <a href="views/sources/adminlte/index3.html" class="navbar-brand">
-        <img src="views/sources/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
-      </a>
+<div class="container py-2 py-lg-4">
+  <div class="row">
 
-      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+    <div class="col-12 col-lg-2 mt-1">
+      <div class="d-flex justify-content-center">
+        <a href="home" class="navbar-brand">
+          <img src="views/sources/img/identidad-corporativa/logo.png" alt="" class="brand-image img-fluid py-3 px-5 p-lg-0 pe-lg-3">
+        </a>
+      </div>
+    </div>
 
-      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-          </li>
-          <li class="nav-item">
-            <a href="index3.html" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Contact</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">Dropdown</a>
-            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
-              <li><a href="#" class="dropdown-item">Some action </a></li>
-              <li><a href="#" class="dropdown-item">Some other action</a></li>
+    <div class="col-12 col-lg-7 col-xl-8 mt-1 px-3 px-lg-0">
+      
+      <a href="#" data-widget="pushmenu" role="button" class="nav-link float-start"> <i class="fas fa-bars"></i></a>
 
-              <li class="dropdown-divider"></li>
+      <!-- Dropdown -->
+      <div class="dropdown px-1 float-start bg-info">
+        
+        <a href="#" id="dropdownSubMenu1" class="nav-link dropdown-toggle text-uppercase" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 
-              <!-- Level two dropdown-->
-              <li class="dropdown-submenu dropdown-hover">
-                <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Hover for action</a>
-                <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
-                  <li>
-                    <a tabindex="-1" href="#" class="dropdown-item">level 2</a>
-                  </li>
+          <span class="d-lg-block d-none">Categoria <i class="ps-lg-2 fas fa-th-list"></i></span>
 
-                  <!-- Level three dropdown-->
-                  <li class="dropdown-submenu">
-                    <a id="dropdownSubMenu3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">level 2</a>
-                    <ul aria-labelledby="dropdownSubMenu3" class="dropdown-menu border-0 shadow">
-                      <li><a href="#" class="dropdown-item">3rd level</a></li>
-                      <li><a href="#" class="dropdown-item">3rd level</a></li>
-                    </ul>
-                  </li>
-                  <!-- End Level three -->
+          <span class="d-lg-none d-block"><i class="fas fa-th-list"></i></span>
+        </a>
 
-                  <li><a href="#" class="dropdown-item">level 2</a></li>
-                  <li><a href="#" class="dropdown-item">level 2</a></li>
-                </ul>
+        <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+
+          <!-- Ropa -->
+          <li class="dropdown-submenu dropdown-hover">
+            <a href="#" id="dropdownSubMenu2" class="dropdown-item dropdown-toggle text-uppercase" role="button" data-toggle="dropdown" aria-haspopup="true" aria-extended="false">
+              <i class="fas fa-tshirt pe-2 fa-xs"></i>Ropa
+            </a>
+
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Ropa para dama</a>
               </li>
-              <!-- End Level two -->
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Ropa para hombre</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Ropa deportiva</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Ropa infantil</a>
+              </li>
+
             </ul>
+
+            <ul class="dropdown-menu border-0 shadow" aria-labelledby="dropdownSubMenu2">
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Ropa para dama</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Ropa para hombre</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Ropa deportiva</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Ropa infantil</a>
+              </li>
+            </ul>
+
+
           </li>
+        
+          <!-- Calzado -->
+          <li class="dropdown-submenu dropdown-hover">
+            <a href="#" id="dropdownSubMenu2" class="dropdown-item dropdown-toggle text-uppercase" role="button" data-toggle="dropdown" aria-haspopup="true" aria-extended="false">
+              <i class="fas fa-shoe-prints pe-2 fa-xs"></i>Calzado
+            </a>
+
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Calzado para dama</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Calzado para hombre</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Calzado deportiva</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Calzado infantil</a>
+              </li>
+
+            </ul>
+
+            <ul class="dropdown-menu border-0 shadow" aria-labelledby="dropdownSubMenu2">
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Calzado para dama</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Calzado para hombre</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Calzado deportiva</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Calzado infantil</a>
+              </li>
+            </ul>
+
+
+          </li>
+
+          <!-- Tecnologías -->
+          <li class="dropdown-submenu dropdown-hover">
+            <a href="#" id="dropdownSubMenu2" class="dropdown-item dropdown-toggle text-uppercase" role="button" data-toggle="dropdown" aria-haspopup="true" aria-extended="false">
+              <i class="fas fa-laptop pe-2 fa-xs"></i>Tecnología
+            </a>
+
+            <ul class="border-0 shadow py-3 ps-3 d-block d-lg-none">
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Tecnología móvil</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Tabletas electrónicas</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Computadoras</a>
+              </li>
+              <li>
+                <a tabindex="-1" href="#" class="dropdown-item">Auriculares</a>
+              </li>
+
+            </ul>
+
+            <ul class="dropdown-menu border-0 shadow" aria-labelledby="dropdownSubMenu2">
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Tecnología móvil</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Tabletas electrónicas</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">computadoras</a>
+              </li>
+              <li>
+                <a href="#" tabindex="-1" class="dropdown-item">Auriculares</a>
+              </li>
+            </ul>
+
+
+          </li>
+
         </ul>
 
-        <!-- SEARCH FORM -->
-        <form class="form-inline ml-0 ml-md-3">
-          <div class="input-group input-group-sm">
-            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-              <button class="btn btn-navbar" type="submit">
-                <i class="fas fa-search"></i>
-              </button>
-            </div>
-          </div>
-        </form>
       </div>
 
-      <!-- Right navbar links -->
-      <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
-        <!-- Messages Dropdown Menu -->
-        <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
-            <i class="fas fa-comments"></i>
-            <span class="badge badge-danger navbar-badge">3</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
-              <div class="media">
-                <img src="views/sources/adminlte/dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-                <div class="media-body">
-                  <h3 class="dropdown-item-title">
-                    Brad Diesel
-                    <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                  </h3>
-                  <p class="text-sm">Call me whenever you can...</p>
-                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                </div>
-              </div>
-              <!-- Message End -->
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
-              <div class="media">
-                <img src="views/sources/adminlte/dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                <div class="media-body">
-                  <h3 class="dropdown-item-title">
-                    John Pierce
-                    <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                  </h3>
-                  <p class="text-sm">I got your message bro</p>
-                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                </div>
-              </div>
-              <!-- Message End -->
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
-              <div class="media">
-                <img src="views/sources/adminlte/dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                <div class="media-body">
-                  <h3 class="dropdown-item-title">
-                    Nora Silvester
-                    <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                  </h3>
-                  <p class="text-sm">The subject goes here</p>
-                  <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                </div>
-              </div>
-              <!-- Message End -->
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+      <!-- buscador -->
+      <form class="form-inline">
+        <div class="input-group w-100 me-0 me-lg-4">
+          <input type="search" class="form-control rounded-0 p-3 pe-5" placeholder="Buscar..." style="height:40px">
+          <div class="input-group-append px-2 bg-info">
+            <button class="btn btn-navbar text-white" type="submit">
+              <i class="fas fa-search"></i>
+            </button>
           </div>
-        </li>
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
-            <i class="far fa-bell"></i>
-            <span class="badge badge-warning navbar-badge">15</span>
-          </a>
-          <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <span class="dropdown-header">15 Notifications</span>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-envelope mr-2"></i> 4 new messages
-              <span class="float-right text-muted text-sm">3 mins</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-users mr-2"></i> 8 friend requests
-              <span class="float-right text-muted text-sm">12 hours</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <i class="fas fa-file mr-2"></i> 3 new reports
-              <span class="float-right text-muted text-sm">2 days</span>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-            <i class="fas fa-th-large"></i>
-          </a>
-        </li>
-      </ul>
+        </div>
+      </form>
+    
     </div>
-  </nav>
-  <!-- /.navbar -->
+
+    <!-- cesta -->
+    <div class="col-12 col-lg-3 col-xl-2 mt-1 px-3 px-lg-0">
+      <div class="my-2 my-lg-0 d-flex justify-content-center">
+        
+        <a href="#">
+          <button class="btn btn-default float-start rounded-0 border-0 py-2 px-3 bg-info">
+            <i class="fa fa-shopping-cart"></i>
+          </button>
+        </a>
+
+        <div class="small border float-start ps-2 pe-5 w-100">
+          TU CESTA <span>0</span><br> USD $ <span>0</span>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+</div>
