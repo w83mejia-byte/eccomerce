4a764ce7831b003445590144dc40920d0a1ddc0c
William Mejía <w83mejia@gmail.com>
2025-10-09 15:27:38 -0500

    agregados productos más vistos y más vendidos

--- views/pages/home/home.php
+++ views/pages/home/home.php
@@ -3,4 +3,7 @@
     include "partials/slide.php";
     include "partials/banner.php";
     include "partials/free.php";
+    include "partials/most-seen.php";
+    include "partials/most-sold.php";
+
 ?>
---
+++ views/pages/home/partials/most-seen.php
@@ -0,0 +1,179 @@
+<div class="container-fluid bg-light border">
+
+    <div class="container clearfix">
+        <div class="btn-group float-end p-2">
+            <button class="btn btn-default btnView bg-info" attr-type="grid" attr-index="2">
+                <i class="fas fa-th fa-xs pe-1"></i>
+                <span class="col-xs-0 float-end small mt-1">GRID</span>
+            </button>
+
+            <button class="btn btn-default btnView" attr-type="list" attr-index="2">
+                <i class="fas fa-list fa-xs pe-1"></i>
+                <span class="col-xs-0 float-end small mt-1">LIST</span>
+            </button>
+        </div>
+    </div>
+
+</div>
+
+<div class="container-fluid bg-white">
+    <div class="container">
+        <div class="clearfix pt-3 pb-0 px-2">
+            <h4 class="float-start text-uppercase pt-2">Artículos más vistos</h4>
+            <span class="float-end">
+                <a href="#" class="btn btn-default bg-info">
+                    <small>VER MÁS <i class="fas fa-chevron-right"></i></small>
+                </a>
+            </span>
+        </div>
+
+        <hr style="color:#555">
+        
+        <!-- GRID -->
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 pt-3 pb-4 grid-2">
+
+            <!-- tarjeta 1 -->
+            <div class="col px-3 py-2 py-lg-0">
+                <a href="#">
+                    <figure class="imgProduct">
+                        <img src="views/sources/img/products/ropa/1/ropa01.jpg" alt="ropa" class="img-fluid">
+                    </figure>
+
+                    <h5>
+                        <small class="text-uppercase text-muted">Falda de Flores</small>
+                    </h5>
+                </a>
+
+                <h6>
+                    <span class="badge badgeNew bg-warning text-uppercase text-white mt-1 p-2">Nuevo</span>
+                </h6>
+
+                <div class="clearfix">
+                    <h5 class="float-start text-uppercase text-muted">
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+                    </h5>
+
+                    <span class="float-end">
+                        <div class="btn-group btn-group-sm">
+                            <button class="btn btn-light border">
+                                <i class="fas fa-heart"></i>
+                            </button>
+                            <button class="btn btn-light border">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                        </div>
+                    </span>
+                </div>
+
+            </div>
+
+            <!-- tarjeta 2 -->
+            <div class="col px-3 py-2 py-lg-0">
+                <a href="#">
+                    <figure class="imgProduct">
+                        <img src="views/sources/img/products/ropa/2/ropa02.jpg" alt="ropa" class="img-fluid">
+                    </figure>
+
+                    <h5>
+                        <small class="text-uppercase text-muted">Vestido jean</small>
+                    </h5>
+                </a>
+
+                <h6>
+                    <span class="badge badgeNew bg-warning text-uppercase text-white mt-1 p-2">Nuevo</span>
+                </h6>
+
+                <div class="clearfix">
+                    <h5 class="float-start text-uppercase text-muted">
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+                    </h5>
+
+                    <span class="float-end">
+                        <div class="btn-group btn-group-sm">
+                            <button class="btn btn-light border">
+                                <i class="fas fa-heart"></i>
+                            </button>
+                            <button class="btn btn-light border">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                        </div>
+                    </span>
+                </div>
+
+            </div>
+
+            <!-- tarjeta 3 -->
+            <div class="col px-3 py-2 py-lg-0">
+                <a href="#">
+                    <figure class="imgProduct">
+                        <img src="views/sources/img/products/ropa/3/ropa03.jpg" alt="ropa" class="img-fluid">
+                    </figure>
+
+                    <h5>
+                        <small class="text-uppercase text-muted">Top Dama</small>
+                    </h5>
+                </a>
+
+                <h6>
+                    <span class="badge badgeNew bg-warning text-uppercase text-white mt-1 p-2">Nuevo</span>
+                </h6>
+
+                <div class="clearfix">
+                    <h5 class="float-start text-uppercase text-muted">
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+                    </h5>
+
+                    <span class="float-end">
+                        <div class="btn-group btn-group-sm">
+                            <button class="btn btn-light border">
+                                <i class="fas fa-heart"></i>
+                            </button>
+                            <button class="btn btn-light border">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                        </div>
+                    </span>
+                </div>
+
+            </div>
+            
+            <!-- tarjeta 4 -->
+            <div class="col px-3 py-2 py-lg-0">
+                <a href="#">
+                    <figure class="imgProduct">
+                        <img src="views/sources/img/products/ropa/4/ropa04.jpg" alt="ropa" class="img-fluid">
+                    </figure>
+
+                    <h5>
+                        <small class="text-uppercase text-muted">Vestido Clásico</small>
+                    </h5>
+                </a>
+
+                <h6>
+                    <span class="badge badgeNew bg-warning text-uppercase text-white mt-1 p-2">Nuevo</span>
+                </h6>
+
+                <div class="clearfix">
+                    <h5 class="float-start text-uppercase text-muted">
+                        <del class="small" style="color:#bbb">USD 129</del> $11
+                    </h5>
+
+                    <span class="float-end">
+                        <div class="btn-group btn-group-sm">
+                            <button class="btn btn-light border">
+                                <i class="fas fa-heart"></i>
+                            </button>
+                            <button class="btn btn-light border">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                        </div>
+                    </span>
+                </div>
+
+            </div>
+
+        </div>
+
+        <!-- LIST -->
+    </div>
+</div>
