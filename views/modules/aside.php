434224cd1b71a6d81d8f214f15efd90c9b65cd22
William Mejía <w83mejia@gmail.com>
2025-10-21 14:58:27 -0500

    Agregada las rutas blancas y los enlaces del backoffice

--- views/modules/aside.php
+++ views/modules/aside.php
@@ -35,25 +35,46 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
+          <!-- Administradores -->
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            <a href="/admin/administradores" class="nav-link active">
+              <i class="nav-icon fas fa-users-cog"></i>
+              <p>
+                Administradores
+              </p>
+            </a>
+          </li>
+
+          <!-- Dashboard -->
+          <li class="nav-item">
+            <a href="/admin/dashboard" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
+              </p>
+            </a>
+          </li>
+
+          <!-- Productos -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-shopping-bag"></i>
+              <p>
+                Productos
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="views/sources/adminlte/index.html" class="nav-link">
+                <a href="/admin/categorias" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
+                  <p>Categorías</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="views/sources/adminlte/index2.html" class="nav-link">
+                <a href="/admin/subcategorias" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
+                  <p>Subcategorías</p>
                 </a>
               </li>
               <li class="nav-item">
@@ -65,6 +86,39 @@
             </ul>
           </li>
 
+          <!-- Ventas -->
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-funnel-dollar"></i>
+              <p>
+                Ventas
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="/admin/pedidos" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Pedidos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/admin/informes" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Informes</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="/admin/disputas" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Disputas</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+
+          <!-- Widges -->
           <li class="nav-item">
             <a href="../widgets.html" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
@@ -75,6 +129,7 @@
             </a>
           </li>
 
+          <!-- Cerrar Sesión -->
           <li class="nav-item">
             <a href="/salir" class="nav-link bg-danger">
               <i class="nav-icon fas fa-sign-out-alt"></i>
--- views/pages/admin/admin.php
+++ views/pages/admin/admin.php
@@ -3,5 +3,21 @@
 if(!isset($_SESSION['admin'])){
     include "login/login.php";
 }else{
-    include "dashboard/dashboard.php";
+
+    if(
+        $arrayRutas[1] === "administradores" ||
+        $arrayRutas[1] === "categorias" ||
+        $arrayRutas[1] === "subcategorias" ||
+        $arrayRutas[1] === "inventario" ||
+        $arrayRutas[1] === "mensajes" ||
+        $arrayRutas[1] === "pedidos" ||
+        $arrayRutas[1] === "informes" ||
+        $arrayRutas[1] === "disputas" ||
+        $arrayRutas[1] === "clientes" ||
+        $arrayRutas[1] === "dashboard"
+
+    ){
+        include $arrayRutas[1]."/".$arrayRutas[1].".php";
+    }
+
 }
