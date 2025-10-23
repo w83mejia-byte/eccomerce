c318dc9fda7f6f237cbeb46576c5d7ab62c9b6b3
William Mejía <w83mejia@gmail.com>
2025-10-23 16:12:36 -0500

    Formulario registro

--- views/pages/admin/administradores/modulos/creacion.php
+++ views/pages/admin/administradores/modulos/creacion.php
@@ -1,1 +1,38 @@
-Creación
+<div class="content">
+    <div class="container">
+        <div class="card">
+            <form class="needs-validations" method="post" novalidate>
+                <div class="card-header">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 text-center text-lg-left">
+                                <h4 class="mt-3">Agregar Administrador</h4>
+                            </div>
+
+                            <div class="col-12 col-lg-6 mt-2 d-none d-lg-block">
+                                <button type="submit" class="btn border-0 bg-principal float-right py-2 px-3 btn-sm rounded-pill">Registrar</button>
+
+                                <a href="/admin/administradores" class="btn btn-sm btn-default float-right py-2 px-3 mr-2 rounded-pill">Regresar</a>
+                            </div>
+
+                            <div class="col-12 text-center d-flex justify-content-center mt-2 d-block d-lg-none">
+                                <div>
+                                    <a href="/admin/administradores" class="btn btn-xs btn-default py-2 px-3 rounded-pill mr-2">Regresar</a>
+                                </div>
+
+                                <div>
+                                    <button class="btn border-0 bg-principal py-2 px-3 btn-xs rounded pill" type="submit">Registrar</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card-body"></div>
+
+                <div class="card-footer"></div>
+            </form>
+        </div>
+    </div>
+</div>
